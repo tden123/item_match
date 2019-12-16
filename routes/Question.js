2a1b54362f754bd86f8f5bc7661cc4995c04f519
Terence Denning <terencedenning@outlook.com>
2019-12-16 16:26:51 +1100

    save question to db

--- routes/Question.js
+++ routes/Question.js
@@ -1,8 +1,11 @@
 const Router = require("koa-router");
 const router = new Router({ prefix: '/question' });
+const Question = require('../models/Question');
 
-router.post('/create', (ctx, next) => {
+router.post('/create', async (ctx, next) => {
+    const { question, options } = ctx.request.body;
     console.log(ctx.request.body);
+    await new Question({ question, options }).save();
 });
 
 module.exports = router;
