77323abd8271f9aa5e5b156f824c86da5a4da6af
Terence Denning <terrencedenning@gmail.com>
2019-12-19 19:17:00 +1100

    quiz added

--- routes/Quiz.js
+++ routes/Quiz.js
@@ -2,20 +2,23 @@ const Router = require('koa-router');
 const router = new Router({ prefix: '/quiz' });
 const User = require('../models/User');
 const Question = require('../models/Question');
+const Quiz = require('../models/Quiz');
 
 // @route   POST api/question/create
 // @desc    Create a new question and store in user
 // @access  Private
 router.post('/create_quiz', async (ctx, next) => {
-  const user = await User.findOne({ shop: ctx.session.shop });
-  /*
-  ctx.request.body.map(_id => {
-    let question = Question.findOne({ _id });
-    if (question) user.quizes.push(question);
+  //const user = await User.findOne({ shop: ctx.session.shop });
+  const newQuiz = await new Quiz({ name: 'new quiz', questions: [] });
+
+  ctx.request.body.map(async _id => {
+    const q = await Question.findOne({ _id });
+    newQuiz.questions.push(q.question);
+    console.log(q);
   });
-  await user.save();
-  */
-  // Cyclic dependancy here as question is within 2 models
+
+  await newQuiz.save();
+  console.log('new quiz created...');
 });
 
 module.exports = router;
